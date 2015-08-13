e03083b57f71f242b127e3caabe1366c6d0d503a
Derek Duncan <mail-spam@derekduncan.me>
2015-08-13 01:15:17 -0500

    emblem again...

--- migrations/1439440247628-generate-new-emblems.js
+++ migrations/1439440247628-generate-new-emblems.js
@@ -7,7 +7,7 @@ exports.up = function(next) {
   User.find({}).exec(function(err, users) {
     if (err) return done(err)
     async.eachSeries(users, function(user, done) {
-      if (user.scope === 'admin') return done();
+      //if (user.scope === 'admin') return done();
 
       var newUpload = new Upload({
         filename: user.first_name.toLowerCase() + '.png'
@@ -17,13 +17,15 @@ exports.up = function(next) {
         name: user.full_name
       };
       newUpload.createUserEmblem(emblemOptions, function(err, emblemOptions) {
+        if (err) return done(err);
         user.emblem = emblemOptions;
-        user.save(function() {
-          return done();
+        user.save(function(err) {
+          return done(err);
         });
       })
     }, function(err) {
       console.log( 'MIGRATION COMPLETE' );
+      console.log(err)
       return next(err);
     });
   });
